cf083ee27b95eb549df61389324606e6f7adcf2c
awaわ <a970565@163.com>
2024-09-06 00:55:27 +0800

    (

--- src/Application.cpp
+++ src/Application.cpp
@@ -1,5 +1,8 @@
 #include <ConsoleRealm.h>
 
-static int mian() {
-	Realm::ConsoleRealm();
+int main() {
+	dpp::cluster bot("MTE0MDE2NjU3ODE3NzA2OTA3Nw.Gkh0eS.8l_NIfiGGGs61oQPnorpPTGJTS7QNvTSzDpVXc");
+	bot.on_log(dpp::utility::cout_logger());
+	bot.start(dpp::st_wait);
+	return 9;
 }
