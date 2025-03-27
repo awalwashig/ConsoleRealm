e74bd63d183736dd675094f6628e0f021e38fc44
awaわ <awalwashig@gmail.com>
2025-03-27 22:55:44 +0800

    update

--- src/Application.cpp
+++ src/Application.cpp
@@ -1,62 +1,362 @@
 ﻿#include "Application.h"
-#include "base/Config.h"
 
-#include "RealmDC/RealmDC.h"
-#include "RealmDC/LinkQQ.h"
+std::unique_ptr<Realm> Realm::m_instance;
 
-#include "RealmQQ/RealmQQ.h"
-#include "RealmQQ/LinkDC.h"
+int main() {
+	std::string path{ "/home/woomy/projects/ConsoleApplication/data/config.json" };
 
-namespace Realm {
-	//Necessary initialization
-	Application* Application::Init() {
-		Base::InitConfig("/home/awalwa/projects/ConsoleApplication/date/");
-
-		InitRealmDC();
-		InitRealmQQ();
-
-		RealmDC::GetInstance()->DCInit();
-		RealmQQ::GetInstance()->QQInit();
-
-		//Link
-		DC::LinkQQ::InitLink();
-		QQ::LinkDC::InitLink();
-
-		return s_Instance;
-	}
-
-	//start Run and Execute Funtion,Runing date link
-	Application* Application::Run() {
-		RealmDC::GetInstance()->DCRun();
-		RealmQQ::GetInstance()->QQRun();
-
-		return s_Instance;
-	}
-
-	//~Funtion,And Save Run Data
-	Application* Application::Quit() {
-		RealmDC::GetInstance()->DCQuit();
-		RealmQQ::GetInstance()->QQQuit();
-
-		return s_Instance;
-	}
-
-	//TODO:User defined
-	Application::Application() {
-
-	}
-
-	//Practice Free memory
-	Application::~Application() {
-		delete s_Instance;
-		s_Instance = nullptr;
-	}
-
-	//CreateInstance
-	Application* Application::CreateApplication() {
-		s_Instance = new Application();
-		return s_Instance;
-	}
-
-	Application* Application::s_Instance = nullptr;
+	Realm::m_instance.reset(new Realm);
+	Realm::m_instance->
+		reset(path)
+		.start();
 }
+
+config::config(std::string& path) {
+	reset(path);
+}
+
+config& config::reset(std::string& path) {
+	std::ifstream File(path);
+
+	if (!File.is_open()) {
+		std::cerr << "[ERROR]:Cennt open file" << std::endl;
+	}
+	else {
+		nlohmann::json root;
+		File >> root;
+		m_config.reset(new nlohmann::json(std::move(root)));
+	}
+
+	return *this;
+}
+
+nlohmann::json& config::GetConifg() {
+	return *m_config;
+}
+
+std::string markdown::MarkdownRemove(std::string str) {
+	std::vector<std::tuple<std::string, std::string, std::string>> regexReplacements = {
+			{ R"(\*\*([^*]+)\*\*)", "$1","(**)"},      // Markdown 加粗，如 **加粗** → 保留内部内容
+			{ R"(\*([^*]+)\*)", "$1","(*)" },          // Markdown 斜体，如 *斜体* → 保留内部内容
+			{ R"(__([^_]+)__)", "$1","(__)" },          // Markdown 下划线，如 __下划线__ → 保留内部内容
+			{ R"(~~([^~]+)~~)", "$1","(~~)" },          // Markdown 删除线，如 ~~删除线~~ → 保留内部内容
+			{ R"(\|\|([^|]+)\|\|)", "$1","(||)" },       // 剧透文本，如 ||剧透内容|| → 保留内部内容
+			{ R"(<@!?(\d+)>)", "","" },         // 用户提及，如 <@123456789> 或 <@!987654321> → 保留数字 ID
+			{ R"(<@&(\d+)>)", "","" },           // 角色提及，如 <@&111222333> → 保留数字 ID
+			{ R"(<#(\d+)>)", "","" },            // 频道提及，如 <#444555666> → 保留数字 ID
+	};
+
+	std::string tmp;
+	for (const auto& Obj : regexReplacements) {
+		auto [regex, rp, flag] = Obj;
+
+		tmp = str;
+		std::regex pattern(regex);
+		str = std::regex_replace(str, pattern, rp);
+		if (tmp.size() != str.size()) {
+			Flag.push_back(flag);
+		}
+	}
+	return str;
+}
+
+std::string markdown::MarkdownAttachedQQ(std::string&& str) {
+	for (const auto& Obj : Flag) {
+		str = str + Obj;
+		std::cout << str << std::endl;
+	}
+	return str;
+}
+
+
+void utility::get_http(nlohmann::json& jsonDate, std::string url) {
+	std::string jsonStr = jsonDate.dump();
+
+	// 初始化 libcurl
+	curl_global_init(CURL_GLOBAL_DEFAULT);
+	CURL* curl = curl_easy_init();
+	if (curl) {
+		// 设置请求头为 JSON 格式
+		struct curl_slist* headers = nullptr;
+		headers = curl_slist_append(headers, "Content-Type: application/json");
+
+		// 设置请求 URL 和 POST 数据
+		curl_easy_setopt(curl, CURLOPT_URL, url.c_str());
+		curl_easy_setopt(curl, CURLOPT_HTTPHEADER, headers);
+		curl_easy_setopt(curl, CURLOPT_POSTFIELDS, jsonStr.c_str());
+
+		// 执行请求
+		CURLcode res = curl_easy_perform(curl);
+		if (res != CURLE_OK) {
+			std::cerr << "请求失败: " << curl_easy_strerror(res) << std::endl;
+		}
+		else {
+
+		}
+
+		// 清理资源
+		curl_slist_free_all(headers);
+		curl_easy_cleanup(curl);
+	}
+	else {
+		std::cerr << "初始化 libcurl 失败" << std::endl;
+	}
+	curl_global_cleanup();
+}
+
+qq& qq::reset(nlohmann::json& config) {
+	this->config = config["qq"];
+
+	m_qq = twobot::BotInstance::createInstance(
+		twobot::Config{
+			this->config["ip"],
+			this->config["http"],
+			this->config["ws"],
+			this->config["token"]
+		}
+	);
+
+	main();
+
+	return *this;
+}
+
+qq& qq::set_callback(void(*fn)(nlohmann::json)) {
+	callback = fn;
+	return *this;
+}
+
+qq& qq::start() {
+	std::thread([&]() {
+		m_qq->start();
+		}).detach();
+	return *this;
+}
+
+twobot::BotInstance& qq::GetInstance() {
+	return *m_qq;
+}
+
+void qq::accept(nlohmann::json input) {
+	static const std::string url = Realm::m_instance->GetConifg()["qq"]["useip"].get<std::string>() + "/send_group_msg";
+	std::cout << "QQ: accept" << std::endl;
+	utility::get_http(input, url);
+}
+
+qq& qq::main() {
+	using twobot::Config;
+	using twobot::BotInstance;
+	using twobot::ApiSet;
+	using namespace twobot::Event;
+
+	m_qq->onEvent<GroupMsg>([&](const GroupMsg& msg) {
+		std::cout << msg.raw_msg << std::endl;
+
+		if (msg.group_id != config["group"]) {
+			return;
+		}
+		nlohmann::json input;
+
+		std::string tmp_message = "";
+
+		//TODO
+		for (auto& data : msg.raw_msg["message"]) {
+			if (data["type"] == "text") {
+				tmp_message += data["data"]["text"].get<std::string>() + "\n";
+
+				continue;
+			}
+
+			if (data["type"] == "image") {
+				tmp_message += get_image_url(data["data"]["file"].get<std::string>()) + "\n";
+
+				continue;
+			}
+
+			//不用处理，webhook不能回复
+			if (data["type"] == "reply") {
+				continue;
+			}
+		}
+
+		input["content"] = std::move(tmp_message);
+		input["username"] = msg.raw_msg["sender"]["nickname"];
+		input["avatar_url"] = std::string("https://q.qlogo.cn/headimg_dl?dst_uin=") + std::string(std::to_string((int)msg.user_id)) + std::string("&spec=2&img_type=jpg");
+
+		callback(input);
+		});
+	return *this;
+}
+
+//看网址托管情况为定
+std::string qq::get_image_url(std::string file_id) {
+	static const std::string prefix = "/root/.config/QQ";
+
+	if (!m_qq->getApiSet().getImage(file_id).second["data"]["file"].is_null()) {
+		std::string message = m_qq->getApiSet().getImage(file_id).second["data"]["file"].get<std::string>();
+
+		//史
+		std::string cmd = "sudo mv -f " + message + " " + message + ".gif";
+		system(cmd.c_str());
+		//End
+
+		message.erase(0, prefix.length());
+		message = config["domain_name"].get<std::string>() + message + ".gif"; //gif为史
+
+		return message;
+	}
+	return std::string();
+}
+
+discord::discord(nlohmann::json& config) {
+	reset(config);
+}
+
+discord& discord::reset(nlohmann::json& config) {
+	this->config = config["discord"];
+	m_cluster.reset(new dpp::cluster(this->config["token"], dpp::i_default_intents | dpp::i_message_content | dpp::i_guild_messages));
+
+	main();
+
+	return *this;
+}
+
+discord& discord::set_callback(void(*fn)(nlohmann::json)) {
+	callback = fn;
+
+	return *this;
+}
+
+discord& discord::start(dpp::start_type start) {
+	//debug
+	m_cluster->on_log(dpp::utility::cout_logger());
+
+	m_cluster->start(start);
+	return *this;
+}
+
+void discord::set_send_flag() {
+	send_flag = 1;
+}
+
+void discord::accept(nlohmann::json input) {
+	std::cout << "discord: accept" << std::endl;
+
+	Realm::m_instance->set_send_flag();
+
+	utility::get_http(input, Realm::m_instance->GetConifg()["discord"]["webhook"]);
+}
+
+discord& discord::main() {
+	m_cluster->on_message_create([&](const dpp::message_create_t& event) {
+		static std::string Obj = "";
+
+		if (event.msg.author.is_bot() || event.msg.channel_id != config["channel"].get<dpp::snowflake>()) {
+			return;
+		}
+
+		nlohmann::json input;
+		input["group"] = Realm::m_instance->GetConifg()["qq"]["group"];
+
+		markdown MK;
+
+		std::string content = MK.MarkdownRemove(event.msg.content);
+		if (Obj != event.msg.author.global_name || send_flag) {
+			content = event.msg.author.global_name + ":" + MK.MarkdownAttachedQQ(std::move(content));
+
+			Obj = event.msg.author.global_name;
+			send_flag = 0;
+		}
+		else {
+			content = MK.MarkdownAttachedQQ(std::move(content));
+		}
+
+		input["message"] = emoji(content);
+
+		if (content != "") {
+			nlohmann::json obj;
+			obj["data"]["text"] = content;
+			obj["type"] = "text";
+			input["message"].push_back(std::move(obj));
+		}
+
+		std::cout << input.dump() << std::endl;
+
+		input["group_id"] = Realm::m_instance->GetConifg()["qq"]["group"];
+		callback(input);
+		});
+
+	return *this;
+}
+
+nlohmann::json discord::emoji(std::string& obj) {
+	static std::unordered_map<std::string, bool> cache;
+	nlohmann::json res;
+	std::smatch match;
+	std::string cmd = "wget ";
+	std::regex pattern("<:[^:]+:(\\d+)>");
+
+	int index = 0;
+	while (std::regex_search(obj, match, pattern)) {
+		res[index] = {
+			{"data", {
+				{"file", ""},
+				{"sub_type", 1},
+				{"summary", "[动画表情]"},
+				{"url", ""}
+			}},
+			{"type", "image"}
+
+		};
+
+		//史
+		if (cache[match[1].str()] == bool()) {
+			cmd = cmd + "https://cdn.discordapp.com/emojis/" + match[1].str() + ".webp";
+			system(std::string("mkdir -p " + config["cache"].get<std::string>()).c_str());
+			system(cmd.c_str());
+			system(std::string("mv -f " + match[1].str() + ".webp " + config["cache"].get<std::string>()).c_str());
+
+			cache[match[1].str()] = true;
+		}
+
+		res[index]["data"]["file"] = match[1].str() + ".webp";
+		res[index]["data"]["url"] = "file://" + config["cache"].get<std::string>() + "/" + match[1].str() + ".webp";
+
+		obj = match.suffix().str();
+		index++;
+	}
+
+	std::cout << res.dump() << std::endl;
+
+	return res;
+}
+
+Realm::Realm(std::string& config) {
+	reset(config);
+}
+
+Realm& Realm::reset(std::string& config) {
+	config::reset(config);
+
+	discord::reset(config::GetConifg())
+		.set_callback(qq::accept);
+	qq::reset(config::GetConifg())
+		.set_callback(discord::accept);
+
+	return *this;
+}
+
+Realm& Realm::start() {
+
+	qq::start();
+	discord::start(dpp::st_wait);
+	return *this;
+}
+
+/*
+* 通用：消息队列，建立好hash表对双方映射;
+*
+* discord：处理正则表达（表情符号）并保存挂在服务上面;
+*
+* qq处理一堆破事分类好qq喂过来的史;
+*/
--- src/Application.h
+++ src/Application.h
@@ -1,27 +1,121 @@
-﻿/*这个是作为一个引入的文件
-*
-*/
+﻿#pragma once
+#include <iostream>
+#include <memory>
+#include <functional>
+#include <regex>
+#include <curl/curl.h>
+#include <unordered_map>
 
-#pragma once
-#include "base/Config.h"
+#include <dpp/dpp.h>
+#include <twobot.hh>
 
-int main(int argc, char** argv);
+class config {
+public:
+	config() = default;
 
-namespace Realm {
+	config(std::string& path);
 
-	class Application {
-	public:
-		Application* Init();
-		Application* Run();
-		Application* Quit();
+	config& reset(std::string& path);
 
-		Application();
-		~Application();
-	private:
-		static Application* CreateApplication();
-		friend int ::main(int argc, char** argv);
-		static Application* s_Instance;
-	private:
+	nlohmann::json& GetConifg();
+private:
+	std::unique_ptr<nlohmann::json> m_config;
+};
 
-	};
-}
+//class make_hash {
+//public:
+//	make_hash();
+//
+//	void reset();
+//
+//	void push(nlohmann::json message);
+//
+//	void update();
+//private:
+//	std::unordered_map<std::string, std::string> hash_map;
+//
+//	std::unique_ptr<make_hash> m_make_hash;
+//};
+
+class markdown {
+public:
+	markdown() = default;
+
+	std::string MarkdownRemove(std::string str);
+
+	std::string MarkdownAttachedQQ(std::string&& str);
+private:
+	std::vector<std::string> Flag;
+};
+
+class utility {
+public:
+	static void get_http(nlohmann::json& jsonDate, std::string url);
+};
+
+class qq {
+public:
+	qq() = default;
+
+	qq& reset(nlohmann::json& config);
+
+	qq& set_callback(void(*fn)(nlohmann::json));
+
+	qq& start();
+
+public:
+	twobot::BotInstance& GetInstance();
+
+	static void accept(nlohmann::json input);
+private:
+	qq& main();
+
+	std::string get_image_url(std::string file_id);
+private:
+	std::function<void(nlohmann::json)> callback;
+
+	std::unique_ptr<twobot::BotInstance> m_qq;
+	nlohmann::json config;
+};
+
+class discord {
+public:
+	discord() = default;
+
+	discord(nlohmann::json& config);
+
+	discord& reset(nlohmann::json& config);
+
+	discord& set_callback(void(*fn)(nlohmann::json));
+
+	discord& start(dpp::start_type start);
+public:
+	void set_send_flag();
+
+	static void accept(nlohmann::json input);
+private:
+	discord& main();
+
+	nlohmann::json emoji(std::string& obj);
+private:
+
+	bool send_flag = 0;
+
+	std::function<void(nlohmann::json)> callback;
+	std::unique_ptr<dpp::cluster> m_cluster;
+	nlohmann::json config;
+};
+
+class Realm : public config, public qq, public discord {
+public:
+	Realm() = default;
+
+	Realm(std::string& config);
+
+	Realm& reset(std::string& config);
+
+	Realm& start();
+
+public:
+	static std::unique_ptr<Realm> m_instance;
+};
